3eb0185b571dd2a41949418d92aeb8d3d177eb43
mark coates <miscm@btinternet.com>
2024-11-08 21:56:16 +0000

    Company Search

--- AngularDemo/src/app/shared/components/company-search/company-search.component.ts
+++ AngularDemo/src/app/shared/components/company-search/company-search.component.ts
@@ -1,13 +1,29 @@
 // Angular modules
 import { Component } from '@angular/core';
 import { NgIf, NgFor } from '@angular/common';
-import {FormControl, ReactiveFormsModule, FormGroup, Validators} from "@angular/forms";
+import {
+  FormControl,
+  ReactiveFormsModule,
+  FormGroup,
+  Validators,
+  ValidatorFn,
+  ValidationErrors,
+  AbstractControl
+} from "@angular/forms";
 
 // Models
 import {Company} from "../../models/company.model";
 
 // Services
 import {CompanyService} from "../../services/company.service";
+
+export function alphanumericValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value;
+      const isValid = /^[a-zA-Z0-9]*$/.test(value);
+      return isValid ? null : { alphanumeric: true };
+    };
+  }
 
 @Component({
   selector: 'app-company-search',
@@ -20,19 +36,32 @@ export class CompanySearchComponent {
     companies: Company[] = [];
     searchForm!: FormGroup;
     isSubmitted: boolean = false;
+    isSearchCompleted: boolean = false;
 
     constructor(private companyService: CompanyService) {
         this.searchForm = new FormGroup({
-            searchTerm: new FormControl('', [Validators.required])
+            searchTerm: new FormControl('', [Validators.required, alphanumericValidator()])
         });
     }
 
     searchCompanies(): void {
       this.isSubmitted = true;
       if(this.searchForm && this.searchForm.valid) {
+        this.isSearchCompleted = false;
         const searchTerm: string = this.searchForm.get('searchTerm')?.value;
+        this.companyService.searchCompanies(searchTerm).subscribe(
+          {next: (response: any) => {
+            if ( response.items &&  response.items.length) {
+              this.companies = response.items;
+            } else {
+              this.companies = [];
+            }
+            this.isSearchCompleted = true;
+          },
+            error: (error: any) => {
+            console.log(error);
+             }
+          });
       }
-
     }
-
 }
